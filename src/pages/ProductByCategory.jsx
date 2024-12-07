d0e9f425c7fa11abb6fe6e5044c7d81b0383eba9
Niko Maverick <maverick.nk@gmail.com>
2024-12-07 17:03:27 +0100

    Error ProductByCategory.jsx en import

--- src/pages/ProductByCategory.jsx
+++ src/pages/ProductByCategory.jsx
@@ -1,5 +1,5 @@
 import { useLoaderData } from "react-router-dom";
-import CardProduct from "../components/cardProduct/CardProduct.jsx";
+import CardProduct from "../components/cardProduct/CardProduct.jsx"
 
 const ProductByCategory = () => {
 const {data} = useLoaderData();
